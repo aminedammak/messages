80596a2db68797f2513df8704d071f6e0f83cd99
amine.dammak <amine.dammak@foodomarket.com>
2024-12-31 11:39:58 +0100

    Dependency injection for MessagesService and MessagesRepository

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,11 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
   findOne(id: string) {
     console.log('service find one', id);
     return this.messagesRepo.findOne(id);
